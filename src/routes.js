4062989281c408e93b89f3d122b97386cda9bdd1
Ariya Panna <ariyapanna@outlook.com>
2024-04-21 10:52:00 +0700

    feat: added routes

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: (req, h) => {
+            
+        }
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: (req, h) => {
+
+        }
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: (req, h) => {
+
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: (req, h) => {
+
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: (req, h) => {
+
+        }
+    }
+]
+
+module.exports = routes;
